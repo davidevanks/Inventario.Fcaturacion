f20f3adb2a81da53b44d5be1f6c5f1d1d3bb2bae
Kenneth Gaitán <kenneth.gaitan-ext@dominion-global.com>
2021-04-11 06:57:59 -0600

    Primer commit de primera versión

---
+++ Inventario/Scripts/app/registroempresa.js
@@ -0,0 +1,24 @@
+﻿/*Visualizar imagen*/
+
+$("#imagen").change(function () {
+    let imagen = this.files[0];
+
+    if (imagen["type"] != "image/jpeg" && imagen["type"] != "image/png") {
+        $("#imagen").val("");
+        $(".previsualizar").attr("src", "../Content/img/img_logo/TuLogo.png");
+        alert('Formato de imagen invalido');
+    } else if (imagen["size"] > 2000000) {
+        $("#imagen").val("");
+        $(".previsualizar").attr("src", "../Content/img/img_logo/TuLogo.png");
+        alert('El peso máximo de la imagen es de 200mb');
+    } else
+    {
+        let datosImagen = new FileReader;
+        datosImagen.readAsDataURL(imagen);
+
+        $(datosImagen).on("load", function (event) {
+            let rutaImagen = event.target.result;
+            $(".previsualizar").attr("src", rutaImagen);
+        });
+    }
+});
